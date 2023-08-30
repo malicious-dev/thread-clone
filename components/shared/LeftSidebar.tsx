1f500873405f4772eb375cdef5229b9794b94e4f
Malicious-dev <Programming1hars@gmail.com>
2023-08-30 13:29:26 +0530

    left side bar , bottomsidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
 
 const LeftSidebar = () => {
   const router = useRouter();
@@ -34,6 +35,22 @@ const LeftSidebar = () => {
             </Link>
           );
         })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
